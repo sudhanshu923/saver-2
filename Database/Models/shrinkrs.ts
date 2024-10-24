0e4e6021f636edf7229d87f5f989b7faef6d8f90
Sudhanshu095 <sudhanshuneravati095@gmail.com>
2024-10-24 12:47:33 +0530

    third commit

--- Database/Models/shrinkrs.ts
+++ Database/Models/shrinkrs.ts
@@ -1,19 +1,12 @@
 import mongoose from "mongoose";
 
 
-const stockValueSchema = new mongoose.Schema({
-  date: { type: Date, required: true },
-  open: { type: Number, required: true },
-  high: { type: Number, required: true },
-  low: { type: Number, required: true },
-  close: { type: Number, required: true },
-  volume: { type: Number, required: true }
-});
+
 
 const stockSchema = new mongoose.Schema({
   stockSymbol: { type: String, required: true },
   exchange: { type: String, required: true },
-  values: [stockValueSchema]
+  values: []
 });
 
 
